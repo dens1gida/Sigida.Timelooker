5f0225cda36a3acdc607ff9b451772298767f223
den1sigida@gmail.com <den1sigida@gmail.com>
2022-11-14 19:49:43 +0300

    Добавьте файлы проекта.

---
+++ src/Sigida.Timelooker.Models/Models/Activity.cs
@@ -0,0 +1,10 @@
+﻿namespace Sigida.Timelooker.Models.Models;
+
+public class Activity
+{
+    public string Name { get; init; }
+    public DateTime Start { get; init; }
+    public DateTime End { get; init; }
+    public TimeSpan TotalTime => End - Start;
+
+}
